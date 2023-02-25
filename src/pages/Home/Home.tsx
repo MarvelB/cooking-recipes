d6721649736f19171aa06cf5bec8b2581e782e72
Bryan <bryan@devlift.io>
2023-02-25 16:04:38 -0500

    added useFetch hook. Updated home component to pull all recipes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,19 @@
+import useFetch from 'hooks/useFetch';
+import { RecipeModel } from 'types';
 import './Home.css';
 
 interface HomeProps {}
 
 const Home = ({ }: HomeProps) => {
+
+  const { data: recipes, isLoading, error } = useFetch<RecipeModel[]>("http://localhost:8000/recipes");
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isLoading && <p className="loading">Loading...</p>}
+      {recipes && recipes.map((recipe) => (
+        <h2 key={recipe.id}>{recipe.title}</h2>
+      ))}
     </div>
   );
 }
