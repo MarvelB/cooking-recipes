24f486354cdecaf9e45c2df6fa49c661681eabea
Bryan <bryan@devlift.io>
2023-02-27 15:03:27 -0500

    updated hook to be able to deal with post requests

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,44 +4,78 @@ export interface UseFetchType<T> {
     data: T;
     isLoading: boolean;
     error: string;
+    postData: (postData: T) => void;
 }
 
-const useFetch = <T>(url: string): UseFetchType<T> => {
+interface PostDataOptions {
+    method?: string;
+    headers?: HeadersInit;
+    body?: BodyInit;
+}
+
+const useFetch = <T>(url: string, method = "GET"): UseFetchType<T> => {
     const [data, setData] = useState<T>();
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [error, setError] = useState<string>('');
+    const [options , setOptions] = useState<PostDataOptions>({});
+
+    const postData = (postData: T) => {
+        setOptions({
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postData)
+        })
+    }
 
     // Empty array dependency, use effect runs only once
     useEffect(() => {
         const abortCntrl = new AbortController();
 
-        fetch(url, { signal: abortCntrl.signal })
-        .then((res: Response) => {
-            if(!res.ok) {
-                throw Error('Could not fetch the data for that resource');
+        const fetchData = async () => {
+            setIsLoading(true);
+      
+            try {
+              const res = await fetch(url, { ...options,signal: abortCntrl.signal, method });
+      
+              if (!res.ok) {
+                throw new Error(res.statusText);
+              }
+              const resData = (await res.json()) as T;
+      
+              setIsLoading(false);
+              setError("");
+              setData(resData);
+            } catch (err: unknown) {
+              setIsLoading(false);
+              let error = "Could not fetch the data";
+      
+              if (err instanceof Error) {
+                error += ": " + err.message;
+              }
+      
+              if (abortCntrl.signal.aborted) {
+                error += ". The fetch was aborted";
+              }
+      
+              setError(error);
             }
-    
-            return res.json();
-        })
-        .then((data: T) => {
-            setData(data);
-            setIsLoading(false);
-            setError('');
-        })
-        .catch((err: TypeError) => {
-            if (err.name === 'AbortError') {
-                console.log('Fetch Aborted');
-            } else {
-                setIsLoading(false);
-                setError(err.message);
-            }
-        });
+        };
+
+        if (method === "GET") {
+            fetchData();
+        }
+
+        if (method === "POST" && options) {
+            fetchData();
+        }
 
         // Clean up function
         return () => abortCntrl.abort();
-    }, [url]);
+    }, [url, options, method]);
 
-    return {data, error, isLoading} as UseFetchType<T>;
+    return {data, error, isLoading, postData} as UseFetchType<T>;
 }
 
 export default useFetch;
