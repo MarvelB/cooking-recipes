4246ae134725fa6353eccf1f7f458dfa5af3976c
Bryan <bryan@devlift.io>
2023-02-27 13:42:16 -0500

    created initial form for adding new recipes

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,11 +1,58 @@
+import { useState } from 'react';
 import './Create.css';
 
 interface CreateProps {}
 
 const Create = ({ }: CreateProps) => {
+
+  const [title, setTitle] = useState<string>("");
+  const [method, setMethod] = useState<string>("");
+  const [cookingTime, setCookingTime] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
   return (
-    <div>
-      <h2>Create</h2>
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        {/* Ingredients section */}
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+      </form>
     </div>
   );
 }
