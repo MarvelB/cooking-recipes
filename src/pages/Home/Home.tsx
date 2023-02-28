bd0533c9cca91100f69560bdd6c042dd21720ee8
Bryan <bryan@devlift.io>
2023-02-28 18:28:20 -0500

    retrieving all recipes from firebase

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import RecipeList from 'components/RecipeList/RecipeList';
-import useFetch from 'hooks/useFetch';
+import { projectFirestore } from 'firebase/config';
+import { useEffect, useState } from 'react';
+// import useFetch from 'hooks/useFetch';
 import { RecipeModel } from 'types';
 import './Home.css';
 
@@ -7,7 +9,35 @@ interface HomeProps {}
 
 const Home = ({ }: HomeProps) => {
 
-  const { data: recipes, isLoading, error } = useFetch<RecipeModel[]>("http://localhost:8000/recipes");
+  // const { data: recipes, isLoading, error } = useFetch<RecipeModel[]>("http://localhost:8000/recipes");
+
+  const [recipes, setRecipes] = useState<RecipeModel[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  
+  useEffect(() => {
+    setIsLoading(true);
+
+    projectFirestore.collection("recipes").get().then((recipeSnapshot) => {
+       if (recipeSnapshot.empty) {
+        setError("No recipes to load");
+        setIsLoading(false);
+       } else {
+        let results: RecipeModel[] = [];
+
+        recipeSnapshot.docs.forEach((recipe) => {
+          results.push({id: recipe.id, ...recipe.data()} as RecipeModel);
+        });
+
+        setRecipes(results);
+        setIsLoading(false);
+       }
+    }).catch(err => {
+      setError(err.message);
+    });
+
+  }, []);
+
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
