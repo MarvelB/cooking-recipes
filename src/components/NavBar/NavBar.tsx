c5903f756bf1fae01a3b61285f617e4549ab159a
Bryan <bryan@devlift.io>
2023-02-27 21:27:38 -0500

    added intial code to consume the context

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,18 @@
 import SearchBar from 'components/SearchBar/SearchBar';
+import { ThemeContext } from 'context/ThemeContext';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeContextModel } from 'types';
 import './NavBar.css';
 
 interface NavBarProps {}
 
 const NavBar = ({ }: NavBarProps) => {
+
+  const { color } = useContext<ThemeContextModel | null>(ThemeContext) as ThemeContextModel;
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{background: color}}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Recipes</h1>
